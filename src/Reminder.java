05863ccdb9daeb4b9360109d767cb94c8155a909
gast <gast@10.192.100.85>
2018-12-06 16:31:21 +0100

    Attribute & Methoden erstellt

--- src/Reminder.java
+++ src/Reminder.java
@@ -1,4 +1,38 @@
 
 public class Reminder {
+	private Zeit reminderZeit;
+	private Datum reminderDatum;
+	private Termin termin;
+	private Termin startDatum;
+	private Termin startZeit;
+
+	public Reminder(Datum reminderDatum, Zeit reminderZeit) {
+		this.reminderZeit = reminderZeit;
+		this.reminderDatum = reminderDatum;
+	}
+
+	public Termin geReminder() {
+		return this.termin;
+	}
+
+	public void setMinutenReminder(int minuten) {
+		reminderZeit = Termin.getStartZeit - minuten;
+		reminderDatum = if reminderZeit
+	}
+
+	public void setStundenReminder(int stunden) {
+		reminderZeit = Termin.getStartZeit - stunden;
+		reminderDatum = if reminderZeit
+	}
+
+	public void setTagReminder(int tag) {
+		reminderZeit = Termin.getStartZeit;
+		reminderDate = Termin.getStartDatum - tag;
+	}
+
+	public void setMonatReminder(int monat) {
+		reminderZeit = Termin.getStartZeit;
+		reminderDate = Termin.getStartDatum - monat;
+	}
 
 }
