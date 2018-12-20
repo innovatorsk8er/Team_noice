8c98a808fc957669ae378a9da743025477d07a3c
PP5614 <PP5614@CHTB6006.D52.tes.local>
2018-12-20 14:12:00 +0100

    Neue Klassen erstellt oder überarbeitet.

---
+++ src/Farbe.java
@@ -0,0 +1,45 @@
+
+public enum Farbe {
+
+	OFFEN(255, 255, 255),
+	FAELLIG(255, 59, 48),
+	ABGESCHLOSSEN(76, 217, 100);
+
+	private final int r;
+	private final int g;
+	private final int b;
+	private final String rgb;
+
+	private Farbe(final int r, final int g, final int b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.rgb = r + ", " + g + ", " + b;
+	}
+	
+	public String getRGB() {
+        return rgb;
+    }
+
+    //You can add methods like this too
+    public int getRed(){
+        return r;
+    }
+
+    public int getGreen(){
+        return g;
+    }
+
+    public int getBlue(){
+        return r;
+    }
+
+    //Or even these
+    public Farbe getColor(){
+        return new Farbe(r,g,b);
+    }
+
+    public int getARGB(){
+        return 0xFF000000 | ((r << 16) & 0x00FF0000) | ((g << 8) & 0x0000FF00) | b;
+    }
+}
