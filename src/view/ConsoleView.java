899b3f3b397ec14fae081b594970166cc44f6310
Ernes <Ernes@192.168.0.52>
2019-01-24 18:50:07 +0100

    Anpassung view

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -5,6 +5,12 @@ package view;
  */
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -26,6 +32,7 @@ public class ConsoleView extends JFrame {
 	private JButton jbtnNeuerTermin;
 	private JButton jbtnTerminBearbeiten;
 	private JPanel profilPane;
+	private JPanel neuerTerminPanel;
 	private JLabel lblVorname;
 	private JLabel lblNachname;
 	private JLabel lblMail;
@@ -36,6 +43,11 @@ public class ConsoleView extends JFrame {
 	private JLabel lblReminder;
 	private JLabel lblStatus;
 	private JButton jbtnProfilLaden;
+	private JButton jbtnTabNeuerTerminSchliessen = new JButton("[X]");
+	private JButton jbtnTabBearbeteTerminSchliessen = new JButton("[X]");
+	private int tabNeuIndex;
+	private JPanel bearbeiteTerminPanel;
+	private int tabBearbeiteIndex;
 
 	/**
 	 * Create the frame.
@@ -53,7 +65,129 @@ public class ConsoleView extends JFrame {
 		initMainContent();
 		setVisible(true);
 	}
+	
+	public void initNeuerTermin() {
+		//Tab-Close Button
+		jbtnTabNeuerTerminSchliessen.setOpaque(false);
+		jbtnTabNeuerTerminSchliessen.setContentAreaFilled(false);
+		jbtnTabNeuerTerminSchliessen.setPreferredSize(new Dimension(45, 15));
+		jbtnTabNeuerTerminSchliessen.setFont(new Font("Arial", Font.BOLD, 10));
+		String titel = "Neuer Termin";
+		neuerTerminPanel = new JPanel();
+		
+		JPanel pnlTab = new JPanel(new GridBagLayout());
+		pnlTab.setOpaque(false);
+		JLabel lblTitle = new JLabel(titel);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 0;
+		
+		pnlTab.add(lblTitle, gbc);
 
+		gbc.gridx++;
+		gbc.weightx = 0;
+		pnlTab.add(jbtnTabNeuerTerminSchliessen, gbc);
+		
+		tabbedPane.addTab(titel, null, neuerTerminPanel, "Erstelle hier ein neuen Termin");
+		tabNeuIndex = tabbedPane.indexOfTab(titel);
+		tabbedPane.setTabComponentAt(tabNeuIndex, pnlTab);
+		tabbedPane.setSelectedIndex(tabNeuIndex);
+		// Neuer Termin erstellen Ansicht
+		neuerTerminPanel.setLayout(null);
+		// Vorname
+		/*neuerTerminPanel = new JLabel("Vorname:");
+		neuerTerminPanel.setBounds(65, 20, 90, 40);
+		profilPane.add(lblVorname);
+		txtVorname = new JTextField();
+		txtVorname.setBounds(168, 20, 190, 40);
+		profilPane.add(txtVorname);
+		// Nachname
+		lblNachname = new JLabel("Nachname:");
+		lblNachname.setBounds(65, 70, 90, 40);
+		profilPane.add(lblNachname);
+		txtNachname = new JTextField();
+		txtNachname.setBounds(168, 70, 190, 40);
+		profilPane.add(txtNachname);
+		// Mail
+		lblMail = new JLabel("E-Mail");
+		lblMail.setBounds(65, 120, 90, 40);
+		profilPane.add(lblMail);
+		txtMail = new JTextField();
+		txtMail.setBounds(168, 120, 190, 40);
+		profilPane.add(txtMail);
+		// Button Bereich
+		jbtnProfilspeichern = new JButton("Speichern");
+		jbtnProfilspeichern.setBounds(168, 170, 190, 30);
+		profilPane.add(jbtnProfilspeichern);
+		// Button Bereich
+		jbtnProfilLaden = new JButton("Laden");
+		jbtnProfilLaden.setBounds(168, 220, 190, 30);*/
+		//profilPane.add(jbtnProfilLaden);
+	}
+
+	public void initBearbeiteTermin() {
+		//Tab-Close Button
+		jbtnTabBearbeteTerminSchliessen.setOpaque(false);
+		jbtnTabBearbeteTerminSchliessen.setContentAreaFilled(false);
+		jbtnTabBearbeteTerminSchliessen.setPreferredSize(new Dimension(45, 15));
+		jbtnTabBearbeteTerminSchliessen.setFont(new Font("Arial", Font.BOLD, 10));
+		String titel = "Bearbeite Termin";
+		bearbeiteTerminPanel = new JPanel();
+		
+		JPanel pnlTab = new JPanel(new GridBagLayout());
+		pnlTab.setOpaque(false);
+		JLabel lblTitle = new JLabel(titel);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 0;
+		
+		pnlTab.add(lblTitle, gbc);
+
+		gbc.gridx++;
+		gbc.weightx = 0;
+		pnlTab.add(jbtnTabBearbeteTerminSchliessen, gbc);
+		
+		tabbedPane.addTab(titel, null, bearbeiteTerminPanel, "Termin wird bearbeitet");
+		tabBearbeiteIndex = tabbedPane.indexOfTab(titel);
+		tabbedPane.setTabComponentAt(tabBearbeiteIndex, pnlTab);
+		tabbedPane.setSelectedIndex(tabBearbeiteIndex);
+		// Neuer Termin erstellen Ansicht
+		bearbeiteTerminPanel.setLayout(null);
+		// Vorname
+		/*neuerTerminPanel = new JLabel("Vorname:");
+		neuerTerminPanel.setBounds(65, 20, 90, 40);
+		profilPane.add(lblVorname);
+		txtVorname = new JTextField();
+		txtVorname.setBounds(168, 20, 190, 40);
+		profilPane.add(txtVorname);
+		// Nachname
+		lblNachname = new JLabel("Nachname:");
+		lblNachname.setBounds(65, 70, 90, 40);
+		profilPane.add(lblNachname);
+		txtNachname = new JTextField();
+		txtNachname.setBounds(168, 70, 190, 40);
+		profilPane.add(txtNachname);
+		// Mail
+		lblMail = new JLabel("E-Mail");
+		lblMail.setBounds(65, 120, 90, 40);
+		profilPane.add(lblMail);
+		txtMail = new JTextField();
+		txtMail.setBounds(168, 120, 190, 40);
+		profilPane.add(txtMail);
+		// Button Bereich
+		jbtnProfilspeichern = new JButton("Speichern");
+		jbtnProfilspeichern.setBounds(168, 170, 190, 30);
+		profilPane.add(jbtnProfilspeichern);
+		// Button Bereich
+		jbtnProfilLaden = new JButton("Laden");
+		jbtnProfilLaden.setBounds(168, 220, 190, 30);*/
+		//profilPane.add(jbtnProfilLaden);
+	}
+	
 	private void initTermineListePane() {
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setViewportView(terminListe);
@@ -187,4 +321,23 @@ public class ConsoleView extends JFrame {
 		return jbtnProfilLaden;
 	}
 
+	public JTabbedPane getTabbedPane() {
+		return tabbedPane;
+	}
+
+	public JButton getJbtnTabNeuerTerminSchliessen() {
+		return jbtnTabNeuerTerminSchliessen;
+	}
+
+	public int getTabNeuIndex() {
+		return tabNeuIndex;
+	}
+
+	public JButton getJbtnTabBearbeteTerminSchliessen() {
+		return jbtnTabBearbeteTerminSchliessen;
+	}
+
+	public int getTabBearbeiteIndex() {
+		return tabBearbeiteIndex;
+	}
 }
