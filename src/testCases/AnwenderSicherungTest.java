cce81347baeedf944a8eb53436098360b3e96b4a
gast <gast@10.192.102.49>
2019-02-01 07:42:46 +0100

    AnwenderSicherungsTest kommentiert

--- src/testCases/AnwenderSicherungTest.java
+++ src/testCases/AnwenderSicherungTest.java
@@ -7,12 +7,16 @@ import enums.AnwenderStatus;
 import junit.framework.TestCase;
 import model.Anwender;
 import model.AnwenderSicherung;
+/**
+ * @author Fatma Gediz 
+ * Test-AnwendersicheurngsKlasse, f\u00fcr Anwender speichern, laden und Anwendersicherungsstatus
+ */
 
 public class AnwenderSicherungTest extends TestCase {
 private AnwenderSicherung anwenderSicherung;
 private Anwender anwender;
 	
-	
+/*Test: Status beim Anlegen bzw. Speichernn des Profils/Anwenders*/
 	@Test
 	 public void testAnwenderSicherung()
 	 {
@@ -22,7 +26,7 @@ private Anwender anwender;
 		 assertEquals("c:\\temp\\TerminListe.noice",
 				 	anwenderSicherung.getFileNamePath());
 	 }
-	
+	/*Test: Status beim Laden des Anwender */	
 	@Test
 	 public void testAnwenderSicherungLaden()
 	 {
@@ -31,7 +35,7 @@ private Anwender anwender;
 		assertEquals(AnwenderSicherungStatus.LADEN_ERFOLGREICH.getAnwenderSicherungsStatus(),
 				anwenderSicherung.getAnwenderSicherungStatus().LADEN_ERFOLGREICH.getAnwenderSicherungsStatus());
 	 }
-
+	/*Test: Staus nach dem die Daten des Anwenders bearbeitet werden*/	
 	@Test
 	public void testAnwenderSicherungSpeichern()
 	{
